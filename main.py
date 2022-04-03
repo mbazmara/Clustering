84a9fed550fd076a9637e9bafdb742d6282ce5c7
mbazmara <47808895+mbazmara@users.noreply.github.com>
2022-04-04 00:26:34 +0430

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title='Clustering your data', page_icon=':chart_with_upwards_trend:', layout='wide')
+st.set_page_config(page_title='Clustering your data', page_icon=':chart_with_upwards_trend:')
 st.title("Clustering any DataSet you want")
 st.markdown('Created by **Mohammad Bazmara**')
 
@@ -33,48 +33,54 @@ def visualize(data, n_clusters,x,y):
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
 
-
-# ---- Input Data ----
-radio_btn = ['Upload your Dataset', 'Mall Customers Dataset']
-input_data = st.sidebar.radio('Select one way for clustering',radio_btn)
-if input_data == 'Upload your Dataset':
-    x = st.sidebar.file_uploader("Pick a file", type=['csv', 'xlsx'])
-    st.sidebar.write('[Download Sample>](https://www.kaggle.com/datasets/uciml/iris/download)')
-    if x is not None:
-        data = pd.read_csv(x)
+try:
+    # ---- Input Data ----
+    left , right = st.columns((3,1))
+    with left:
+        radio_btn = ['Upload your Dataset', 'Mall Customers Dataset']
+        input_data = st.radio('Select one way for clustering',radio_btn)
+    with right:
+        st.image('images/mmb2.jpeg', )
+    if input_data == 'Upload your Dataset':
+        x = st.file_uploader("Pick a file", type=['csv', 'xlsx'])
+        st.write('[Download Sample>](https://www.kaggle.com/datasets/uciml/iris/download)')
+        if x is not None:
+            data = pd.read_csv(x)
+            number_of_instances = data.shape[1]
+            input_data = data
+            numeric_features = list(input_data.select_dtypes(['int', 'float']).columns)
+            categorize(input_data)
+            # ---- SlideBar ----
+            st.subheader("first 5 insctances of your dataset")
+            st.write(data.head(5))
+            left, middle, right = st.columns(3)
+            with left:
+                number_of_clusters = st.slider(label='Number of clusters', min_value=2, max_value=8)
+            with middle:
+                f1 = st.selectbox(label='X Numeric Feature:', options=numeric_features)
+            with right:
+                f2 = st.selectbox(label='Y Numeric Feature:', options=numeric_features)
+            st.write('---')
+            visualize(input_data, number_of_clusters, f1, f2)
+            st.write('---')
+    if input_data == 'Mall Customers Dataset':
+        data = pd.read_csv('Mall_Customers.csv')
         number_of_instances = data.shape[1]
         input_data = data
         numeric_features = list(input_data.select_dtypes(['int', 'float']).columns)
         categorize(input_data)
         # ---- SlideBar ----
-        number_of_clusters = st.sidebar.slider(label='Number of clusters', min_value=2, max_value=8)
-        f1 = st.sidebar.selectbox(label='X Numeric Feature:', options=numeric_features)
-        f2 = st.sidebar.selectbox(label='Y Numeric Feature:', options=numeric_features)
-        st.write('---')
-        with st.container():
-            left, right = st.columns(2)
-            with left:
-                st.subheader("first 5 insctances of your dataset")
-                st.write(data.head(5))
-            with right:
-                visualize(input_data, number_of_clusters, f1, f2)
-        st.write('---')
-if input_data == 'Mall Customers Dataset':
-    data = pd.read_csv('Mall_Customers.csv')
-    number_of_instances = data.shape[1]
-    input_data = data
-    numeric_features = list(input_data.select_dtypes(['int', 'float']).columns)
-    categorize(input_data)
-    # ---- SlideBar ----
-    number_of_clusters = st.sidebar.slider(label='Number of clusters', min_value=2, max_value=8)
-    f1 = st.sidebar.selectbox(label='X Numeric Feature:', options=numeric_features)
-    f2 = st.sidebar.selectbox(label='Y Numeric Feature:', options=numeric_features)
-    st.write('---')
-    with st.container():
-        left, right = st.columns(2)
+        st.subheader("first 5 insctances of your dataset")
+        st.write(data.head(5))
+        left, middle, right = st.columns(3)
         with left:
-            st.subheader("first 5 insctances of your dataset")
-            st.write(data.head(5))
+            number_of_clusters = st.slider(label='Number of clusters', min_value=2, max_value=8)
+        with middle:
+            f1 = st.selectbox(label='X Numeric Feature:', options=numeric_features)
         with right:
-            visualize(input_data, number_of_clusters, f1, f2)
-    st.write('---')
+            f2 = st.selectbox(label='Y Numeric Feature:', options=numeric_features)
+        st.write('---')
+        visualize(input_data, number_of_clusters, f1, f2)
+        st.write('---')
+except ValueError as v:
+    st.write(v)
